8124d9d44b818adc90e4c455852c9d55eb9c42a6
GrandCkay <cf.ckay@gmail.com>
2019-07-25 13:10:37 +0300

    Changed design and added ability to enter command names

--- app/src/main/java/com/example/android/courtcounter2/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter2/MainActivity.java
@@ -2,30 +2,38 @@ package com.example.android.courtcounter2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Point;
 import android.os.Bundle;
-import android.os.ParcelUuid;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.example.android.courtcounter2.R;
 
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
     int scoreTeamB = 0;
+    String finalScore = "Final Score";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("scoreTeamA", scoreTeamA);
+        outState.putInt("scoreTeamB", scoreTeamB);
+    }
+
     /**
      * Displays the given score for Team A + 3 Point.
      */
     public void ForTeamA3 (View View) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
+        displayFinalScore (finalScore);
     }
     /**
      * Displays the given score for Team A + 2 Point.
@@ -33,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     public void ForTeamA2 (View View) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
+        displayFinalScore (finalScore);
     }
     /**
      * Displays the given score for Team A Free Throw.
@@ -40,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     public void ForTeamA1 (View View) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
+        displayFinalScore (finalScore);
     }
 
     /**
@@ -48,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
     public void ForTeamB3 (View View) {
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
+        displayFinalScore (finalScore);
     }
     /**
      * Displays the given score for Team B + 2 Point.
@@ -55,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
     public void ForTeamB2 (View View) {
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
+        displayFinalScore (finalScore);
     }
     /**
      * Displays the given score for Team B Free Throw.
@@ -62,13 +74,18 @@ public class MainActivity extends AppCompatActivity {
     public void ForTeamB1 (View View) {
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
+        displayFinalScore (finalScore);
     }
     /**
      * Displays the given final score.
      */
     public void FinalScore (View View) {
+        EditText editTeamA = findViewById(R.id.editTextTeamA);
+        String editTextNameTeamA = editTeamA.getText().toString();
+        EditText editTeamB = findViewById(R.id.editTextTeamB);
+        String editTextNameTextB = editTeamB.getText().toString();
         String finalScore;
-        finalScore = "NY Knicks: " + scoreTeamA + " - " + scoreTeamB + " :Chicago Bulls";
+        finalScore = editTextNameTeamA + ": " + scoreTeamA + " - " + scoreTeamB + " :" + editTextNameTextB;
         displayFinalScore (finalScore);
         scoreTeamA = 0;
         scoreTeamB = 0;
@@ -79,21 +96,32 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        TextView scoreView = findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
     /**
      * Displays the given score for Team B.
      */
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        TextView scoreView = findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
     /**
      * Displays the given final score.
      */
     public void displayFinalScore(String score) {
-        TextView scoreView = (TextView) findViewById(R.id.final_score);
+        TextView scoreView = findViewById(R.id.final_score);
         scoreView.setText("" + (score));
+    }
+
+
+    @Override
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        scoreTeamA = savedInstanceState.getInt("scoreTeamA");
+        scoreTeamB = savedInstanceState.getInt("scoreTeamB");
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 }
